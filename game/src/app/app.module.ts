8be4050edc146b695940af24da5252203dbe6d85
deepakhr1999 <deepakhr1999@gmail.com>
2020-03-20 16:04:53 +0530

    added styles pink-bluegrey

--- game/src/app/app.module.ts
+++ game/src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    MatSliderModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
