2a951f5ae4b54375c774d6973c333666a85db0fa
deepakhr1999 <deepakhr1999@gmail.com>
2020-03-21 19:57:17 +0530

    nested for loop for cards

--- game/src/app/game/game.component.ts
+++ game/src/app/game/game.component.ts
@@ -5,11 +5,38 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
+
+
 export class GameComponent implements OnInit {
+  name = "Deepak"
+  server = "localhost"
+  cards = [
+      [
+        {suit: 'heart', rank: 'J'},
+        {suit: 'heart', rank: 'Q'},
+        {suit: 'heart', rank: 'K'}
+      ],
+      [
+        {suit: 'diamond', rank: 'A'},
+        {suit: 'diamond', rank: '2'},
+        {suit: 'diamond', rank: '3'}
+      ],
+      [
+        {suit: 'spades', rank: 'A'},
+        {suit: 'spades', rank: '6'},
+        {suit: 'spades', rank: '7'}
+      ],
+      [
+        {suit: 'clubs', rank: '4'},
+        {suit: 'clubs', rank: '5'},
+        {suit: 'clubs', rank: '6'}
+      ],
+    ]
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    
+  }
 }
